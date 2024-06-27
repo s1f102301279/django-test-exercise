058d6e9b6ed6db22524ac24c95f3a42a0d9e71ab
1F10230127 <s1f102301279@iniad.org>
2024-06-27 14:10:30 +0900

    Taskモデルにメソッドを追加

--- todo/tests.py
+++ todo/tests.py
@@ -6,7 +6,7 @@ from todo.models import Task
 class SampleTestCase(TestCase):
     def test_sample1(self):
         self.assertEqual(1+2,3)
-        
+
     def test_create_task1(self):
         due = timezone.make_aware(datetime(2024,6,30,23,59,59))
         task = Task(title="task1",due_at=due)
@@ -25,3 +25,31 @@ class SampleTestCase(TestCase):
         self.assertEqual(task.title, "task2")
         self.assertFalse(task.completed)
         self.assertEqual(task.due_at, None)
+
+    def test_is_overdue_future(self):
+        due = timezone.make_aware(datetime(2024,6,30,23,59,59))
+        current = timezone.make_aware(datetime(2024,6,30,0,0,0))
+        task = Task(title='task1',due_at=due)
+        task.save()
+
+        self.assertFalse(task.is_overdue(current))
+
+    def test_is_overdue_past(self):
+        due = timezone.make_aware(datetime(2024,6,30,23,59,59))
+        current = timezone.make_aware(datetime(2024,7,1,0,0,0))
+        task = Task(title='task1',due_at=due)
+        task.save()
+
+        self.assertEqual(task.is_overdue(current))
+
+    def test_is_overdue_none(self):
+        due = timezone.make_aware(datetime(2024,6,30,23,59,59))
+        current = timezone.make_aware(datetime(2024,7,1,0,0,0))
+        task = Task(title='task1',due_at=None)
+        task.save()
+
+        self.assertFalse(task.is_overdue(current))
+
+
+
+    
